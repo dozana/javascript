1690b5016f77266422ca6bc0d14b63a805267c39
Levan <levan.soroznishvili@gmail.com>
2023-12-04 02:51:17 +0400

    var, let and const

---
+++ fundamentals/03-let-const-and-var/script.js
@@ -0,0 +1,18 @@
+// let is block scope
+let age = 30;
+age = 31; // reesign age variable (mutate)
+console.log(age);
+
+
+const birthYear = 1991;
+// birthYear = 1992;
+// console.log(birthYear);
+ 
+
+// var is function scope (never use var)
+var job = 'Programmer';
+job = 'teacher';
+console.log(job);
+
+lastName = 'Smith';
+console.log(lastName);
