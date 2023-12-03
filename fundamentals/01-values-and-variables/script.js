120a448e6d09024bd6b5f4175e98c26e441b735a
Levan <levan.soroznishvili@gmail.com>
2023-12-04 01:52:31 +0400

    values and variables

---
+++ fundamentals/01-values-and-variables/script.js
@@ -0,0 +1,37 @@
+// Values and Variables
+
+console.log(":: Values and Variables ::");
+
+// Variables with Camel Case
+let firstName = "John";
+let lastName = "Doe";
+let age = 30;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+// Varialbes with Underscore
+let full_name = "Tom Jones";
+console.log(full_name);
+
+// Reserved keywords output
+let _function = "Function ABC";
+console.log(_function);
+
+let $function = "Function DFG";
+console.log($function);
+
+// Pascal Case
+let NewRules = "No Rules";
+console.log(NewRules);
+
+// Constants with uppercase
+let PI = 3.1415;
+console.log(PI);
+
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+console.log(myFirstJob);
+console.log(myCurrentJob);
