682baf117a9eae212efa115740c35f7258e283b9
Levan <levan.soroznishvili@gmail.com>
2023-12-04 12:13:51 +0400

    logical operators

---
+++ fundamentals/05-logical-operators/script.js
@@ -0,0 +1,23 @@
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if(hasDriversLicense && hasGoodVision) {
+//   console.log('Sarah is able to drive');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+const isTired = false; // c
+// console.log(hasDriversLicense || hasGoodVision || isTired);
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+
+if(hasDriversLicense && hasGoodVision && !isTired) {
+  console.log('Sarah is able to drive');
+} else {
+  console.log('Someone else should drive...');
+}
