39cfb56e4e6f8d0175d76f7a5adc26b7fbbdb85a
Levan <levan@Levans-MacBook-Pro.local>
2023-12-05 22:52:11 +0400

    operator precedence

---
+++ fundamentals/05-operator-precedence/script.js
@@ -0,0 +1,13 @@
+
+const now = 2037;
+const ageJohn = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJohn + ageSarah) / 2;
+console.log(ageJohn, ageSarah, averageAge);
