e5ac3d580cc11e0894c3533d4ff5faff20138efe
Levan <levan.soroznishvili@gmail.com>
2023-12-05 23:55:20 +0400

    template literals

---
+++ fundamentals/07-template-literals/script.js
@@ -0,0 +1,15 @@
+// Strings and Template literals
+
+const firstName = 'John';
+const job = 'Teacher';
+const birthYear = 1990;
+
+const john = `I am ${firstName} I am a ${job} and I was born in ${birthYear}`;
+console.log(john);
+
+console.log('Just a regular string...');
+
+console.log(`
+multiple
+line
+string`);
