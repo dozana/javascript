8667f162d1652eb93d1cc2db692056b6054c4516
Levan <levan.soroznishvili@gmail.com>
2023-12-13 23:31:35 +0400

    the switch statement

---
+++ fundamentals/16-the-switch-statement/script.js
@@ -0,0 +1,27 @@
+const day = 'thursday';
+
+switch(day) {
+  case 'monday':
+    console.log('Its monday today');
+    break;
+  case 'tuesday':
+    console.log('Its tuesday');
+    break;
+  case 'wednesday':
+    console.log('Its wednesday');
+    break;
+  case 'tursday':
+    console.log('Its tursday');
+    break;
+  case 'friday':
+    console.log('Its friday');
+    break;
+  case 'saturday':
+    console.log('Its saturday');
+    break;
+  case 'sunday':
+    console.log('Its sunday');
+    break;
+  default:
+    console.log('Not a valid day');
+}
