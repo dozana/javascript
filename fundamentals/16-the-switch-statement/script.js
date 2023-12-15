8fc076cd60fd4f1cb6010a6cdf504afd31c66ed1
Levan <levan.soroznishvili@gmail.com>
2023-12-15 20:09:43 +0400

    switch statement example update

--- fundamentals/16-the-switch-statement/script.js
+++ fundamentals/16-the-switch-statement/script.js
@@ -25,3 +25,22 @@ switch(day) {
   default:
     console.log('Not a valid day');
 }
+
+
+if(day === 'monday') {
+  console.log('Its monday today');
+} else if(day === 'tuesday') {
+  console.log('Its tuesday today');
+} else if(day === 'wednesday') {
+  console.log('Its wednesday today');
+} else if(day === 'tursday') {
+  console.log('Its tursday today');
+} else if(day === 'friday') {
+  console.log('Its friday today');
+} else if(day === 'saturday') {
+  console.log('Its  today');
+} else if(day === 'sunday') {
+  console.log('Its sunday today');
+} else {
+  console.log('Not a valid day');
+}
