961e3f4708d6194692c033f6204fe49d2548e0f2
Levan <levan.soroznishvili@gmail.com>
2023-12-16 22:35:39 +0400

    statement and expression

---
+++ fundamentals/17-statement-and-expression/script.js
@@ -0,0 +1,9 @@
+// difference between statements and expressions
+
+if(23>10) {
+  const str = '23 is bigger'; // 23 is bigger - this is an expression
+}
+
+
+
+const str = '23 is bigger'; // this is statement
